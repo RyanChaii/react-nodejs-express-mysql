95370219fe2122907c1d476aadc7a897b9ca2456
RyanChaii <ryanchaihengee@gmail.com>
2023-01-11 16:58:49 +0800

    Repo at latest & final commit

--- AE3_API_Server/config/database.js
+++ AE3_API_Server/config/database.js
@@ -4,16 +4,16 @@ const cors = require("cors");
 
 // Setting for connection to sql, if virtual not set, use preset
 var connection = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    database: process.env.DB_NAME || "tms_system",
-    password: process.env.DB_PASSWORD || "admin"
+  host: process.env.DB_HOST || "host.docker.internal",
+  user: process.env.DB_USER || "root",
+  database: process.env.DB_NAME || "tms_system",
+  password: process.env.DB_PASSWORD || "admin"
 });
 
 // Connecting to mysql
 connection.getConnection(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database!");
+  if (error) throw error;
+  console.log("Successfully connected to the database!");
 });
 
 // Enable future apps to use the connection
